0a0e10f0cef6853c3ea900af7ef5d816009ff23c
nicolascarnovali <130709632+nicolascarnovali@users.noreply.github.com>
2023-07-14 01:30:37 -0300

    carrito e index mismo nivel

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ let productos = [
     { nombre: "Gancia Americano", precio: 799.99, imagen: "../assets/img/GanciaAmericano.jpg" },
     { nombre: "Campari Aperitivo", precio: 1899.99, imagen: "../assets/img/CampariAperitivo.webp" },
     { nombre: "Gin MG", precio: 5399.99, imagen: "../assets/img/GinMG.webp" },
-    {nombre: "Whisky Jack Daniels", precio: 15999.99, imagen: "../assets/img/WhiskyJackDaniels.webp" },
+    { nombre: "Whisky Jack Daniels", precio: 15999.99, imagen: "../assets/img/WhiskyJackDaniels.webp" },
 ];
 
 // Variables de JS necesarias
@@ -58,6 +58,8 @@ let licorerias = [
     { nombre: "Nuñez", direccion: "Vilela 1930, Ciudad", horario: "9:00 - 21:00" },
     { nombre: "Saavedra", direccion: "Ruiz Huidobro 4570, Ciudad", horario: "11:00 - 23:00" },
 ];
+
+
 
 // Función para verificar la edad
 function verificarEdad(edad) {
@@ -255,27 +257,131 @@ function actualizarCantidad(index, cantidad) {
         }
 
         localStorage.setItem("carrito", JSON.stringify(carrito));
-
-        if (cantidadAnterior >= 5 && cantidad < 5) {
-            mostrarCarrito();
-        } else if (cantidadAnterior < 5 && cantidad >= 5) {
-            mostrarCarrito();
-        } else {
-            let subtotal = carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
-            let descuentoTotal = carrito.reduce((total, producto) => {
-                if (producto.descuento) {
-                    return total + producto.precio * producto.cantidad * 0.2;
-                }
-                return total;
-            }, 0);
-            let total = subtotal - descuentoTotal;
-
-            let totalCompra = document.getElementById("total-compra");
-            totalCompra.innerText = "Total de la compra: $" + total.toFixed(2);
-        }
+        mostrarCarrito();
+        mostrarTotalCompra();
     }
-    mostrarCarrito();
 }
+
+function agregarAlCarrito(producto, cantidad) {
+    let carritoData = localStorage.getItem("carrito");
+    carrito = carritoData ? JSON.parse(carritoData) : [];
+
+    let productoExistente = carrito.find((p) => p.nombre === producto.nombre);
+
+    if (productoExistente) {
+        let cantidadTotal = productoExistente.cantidad + cantidad;
+
+        if (cantidadTotal <= 100) {
+            productoExistente.cantidad = cantidadTotal;
+
+            if (cantidadTotal < 5) {
+                delete productoExistente.descuento;
+            } else if (!productoExistente.descuento) {
+                productoExistente.descuento = true;
+            }
+        } else {
+            alert("No se pueden agregar más de 100 unidades del producto: " + producto.nombre);
+            return;
+        }
+    } else {
+        producto.cantidad = cantidad;
+        carrito.push(producto);
+    }
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+
+    alert("Se ha agregado " + cantidad + " unidad(es) del producto '" + producto.nombre + "' al carrito.");
+
+    mostrarCarrito();
+    mostrarTotalCompra();
+}
+
+function mostrarCarrito() {
+    let carritoContainer = document.getElementById("carrito-container");
+    let carritoList = document.getElementById("carrito-list");
+    let totalCompra = document.getElementById("total-compra");
+    carritoList.innerHTML = "";
+    totalCompra.innerText = "";
+    let carritoData = localStorage.getItem("carrito");
+    let carrito = carritoData ? JSON.parse(carritoData) : [];
+
+    if (carrito.length === 0) {
+        carritoContainer.innerText = "El carrito está vacío.";
+    } else {
+        let subtotal = 0;
+        let descuentoTotal = 0;
+
+        for (let i = 0; i < carrito.length; i++) {
+            let producto = carrito[i];
+            let productoDiv = document.createElement("div");
+            productoDiv.classList.add("producto-carrito");
+
+            let productoImagen = document.createElement("img");
+            productoImagen.src = producto.imagen;
+            productoImagen.alt = producto.nombre;
+            productoImagen.classList.add("imagen-producto-carrito");
+
+            let productoNombre = document.createElement("h4");
+            productoNombre.innerText = producto.nombre;
+            productoNombre.classList.add("nombre-producto");
+
+            let cantidadLabel = document.createElement("label");
+            cantidadLabel.innerText = "Cantidad:";
+            cantidadLabel.classList.add("cantidad-label");
+
+            let cantidadSelect = document.createElement("input");
+            cantidadSelect.type = "number";
+            cantidadSelect.min = "1";
+            cantidadSelect.max = "100";
+            cantidadSelect.value = producto.cantidad;
+            cantidadSelect.classList.add("cantidad-select");
+            cantidadSelect.addEventListener("change", function () {
+                actualizarCantidad(i, parseInt(cantidadSelect.value));
+            });
+
+            let productoPrecio = document.createElement("p");
+            let descuento = 0;
+
+            if (producto.descuento) {
+                let descuentoPorUnidad = producto.precio * 0.2;
+                descuento = descuentoPorUnidad * producto.cantidad;
+                productoPrecio.innerText = "Precio unitario: $" + producto.precio.toFixed(2) + " (Descuento: $" + descuento.toFixed(2) + ")";
+                descuentoTotal += descuento;
+            } else {
+                productoPrecio.innerText = "Precio unitario: $" + producto.precio.toFixed(2);
+            }
+
+            let subtotalProducto = producto.precio * producto.cantidad - descuento;
+            subtotal += subtotalProducto;
+
+            let productoSubtotal = document.createElement("p");
+            productoSubtotal.innerText = "Subtotal: $" + subtotalProducto.toFixed(2);
+            productoSubtotal.classList.add("subtotal-producto");
+
+            let eliminarProductoButton = document.createElement("button");
+            eliminarProductoButton.innerText = "Eliminar";
+            eliminarProductoButton.classList.add("boton-eliminar");
+            eliminarProductoButton.addEventListener("click", function () {
+                eliminarProductoDelCarrito(i);
+            });
+
+            productoDiv.appendChild(productoImagen);
+            productoDiv.appendChild(productoNombre);
+            productoDiv.appendChild(cantidadLabel);
+            productoDiv.appendChild(cantidadSelect);
+            productoDiv.appendChild(productoPrecio);
+            productoDiv.appendChild(productoSubtotal);
+            productoDiv.appendChild(eliminarProductoButton);
+
+            carritoList.appendChild(productoDiv);
+        }
+
+        let total = subtotal - descuentoTotal;
+
+        totalCompra.innerText = "Total de la compra: $" + total.toFixed(2);
+    }
+}
+
 
 function mostrarTotalCompra() {
     let carrito = JSON.parse(localStorage.getItem("carrito"));
@@ -299,6 +405,7 @@ function mostrarTotalCompra() {
         let totalCompra = document.getElementById("total-compra");
         totalCompra.innerText = "Total de la compra: $" + total.toFixed(2);
 
+        document.getElementById("finalizar-compra-btn").style.display = "none";
         mostrarSeleccionLicoreria();
     } else {
         alert("El carrito está vacío. Agregue productos antes de finalizar la compra.");
@@ -334,13 +441,23 @@ function mostrarSeleccionLicoreria() {
     licoreriaSeleccionada.appendChild(siguienteBtn);
 }
 
+// Mostrar el carrito al cargar la página
+window.addEventListener("load", function () {
+    mostrarCarrito();
+    document.getElementById("finalizar-compra-btn").addEventListener("click", function () {
+        mostrarTotalCompra();
+        document.getElementById("licoreria-seleccionada").style.display = "block";
+    });
+});
+
+
 function mostrarDatosLicoreria(licoreria) {
     let datosLicoreria = document.createElement("div");
     datosLicoreria.innerHTML = `
-        <h3>Licorería seleccionada para la entrega:</h3>
-        <p><strong>Nombre:</strong> ${licoreria.nombre}</p>
-        <p><strong>Dirección:</strong> ${licoreria.direccion}</p>
-        <p><strong>Horario:</strong> ${licoreria.horario}</p>
+      <h3>Licorería seleccionada para la entrega:</h3>
+      <p><strong>Nombre:</strong> ${licoreria.nombre}</p>
+      <p><strong>Dirección:</strong> ${licoreria.direccion}</p>
+      <p><strong>Horario:</strong> ${licoreria.horario}</p>
     `;
     let licoreriaSeleccionada = document.getElementById("licoreria-seleccionada");
     licoreriaSeleccionada.innerHTML = "";
