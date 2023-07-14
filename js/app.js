a2f16becad1df56c6fd6dea551fd76ef7b330b31
nicolascarnovali <130709632+nicolascarnovali@users.noreply.github.com>
2023-07-14 05:02:55 -0300

    imagenes

--- js/app.js
+++ js/app.js
@@ -37,14 +37,14 @@ let carrito = [];
 
 // Objetos de JS
 let productos = [
-    { nombre: "Cerveza Rubia Heineken", precio: 899.99, imagen: "../assets/img/CervezaRubiaHeineken.jpg" },
-    { nombre: "Cerveza Rubia Corona", precio: 799.99, imagen: "../assets/img/CervezaRubiaCorona.jpg" },
-    { nombre: "Vodka Smirnoff", precio: 1499.99, imagen: "../assets/img/VodkaSmirnoff.jpg" },
-    { nombre: "Vodka Sky", precio: 1199.99, imagen: "../assets/img/VodkaSky.webp" },
-    { nombre: "Gancia Americano", precio: 799.99, imagen: "../assets/img/GanciaAmericano.jpg" },
-    { nombre: "Campari Aperitivo", precio: 1899.99, imagen: "../assets/img/CampariAperitivo.webp" },
-    { nombre: "Gin MG", precio: 5399.99, imagen: "../assets/img/GinMG.webp" },
-    { nombre: "Whisky Jack Daniels", precio: 15999.99, imagen: "../assets/img/WhiskyJackDaniels.webp" },
+    { nombre: "Cerveza Rubia Heineken", precio: 899.99, imagen: "https://hiperlibertad.vteximg.com.br/arquivos/ids/158639-1000-1000/Cerveza-rubia-Heineken-710-Cc-C-HEINEKEN-LATA-710-CC-1-1561.jpg?v=637236252247400000" },
+    { nombre: "Cerveza Rubia Corona", precio: 799.99, imagen: "https://d2izjnmtylvtfh.cloudfront.net/21873937-thickbox_default/cerveza-corona-rubia-269ml.jpg" },
+    { nombre: "Vodka Smirnoff", precio: 1499.99, imagen: "https://d2r9epyceweg5n.cloudfront.net/stores/001/590/373/products/d_smirf1-aef2a6579975a035e916227587677140-1024-1024.jpg" },
+    { nombre: "Vodka Sky", precio: 1199.99, imagen: "https://www.rossofinefood.com/2378-large_default/skyy-vodka-1-l.jpg" },
+    { nombre: "Gancia Americano", precio: 799.99, imagen: "https://gobar.vtexassets.com/arquivos/ids/156378/GANICA.jpg?v=636716737494030000" },
+    { nombre: "Campari Aperitivo", precio: 1899.99, imagen: "https://d3ugyf2ht6aenh.cloudfront.net/stores/835/701/products/campari-aperitivo-750ml1-7cafead7a1f8a2358516661026421170-640-0.jpg" },
+    { nombre: "Gin MG", precio: 5399.99, imagen: "https://gobar.vtexassets.com/arquivos/ids/159646/01031900031.jpg?v=638107905491330000" },
+    { nombre: "Whisky Jack Daniels", precio: 15999.99, imagen: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/384/985/products/whisky-jack-daniels-750-ml1-d9b2e5ecffb25327dd16203179065708-1024-1024.webp" },
 ];
 
 // Variables de JS necesarias
@@ -58,8 +58,6 @@ let licorerias = [
     { nombre: "Nuñez", direccion: "Vilela 1930, Ciudad", horario: "9:00 - 21:00" },
     { nombre: "Saavedra", direccion: "Ruiz Huidobro 4570, Ciudad", horario: "11:00 - 23:00" },
 ];
-
-
 
 // Función para verificar la edad
 function verificarEdad(edad) {
@@ -247,7 +245,6 @@ function actualizarCantidad(index, cantidad) {
 
     if (index >= 0 && index < carrito.length) {
         let producto = carrito[index];
-        let cantidadAnterior = producto.cantidad;
         producto.cantidad = cantidad;
 
         if (cantidad >= 5) {
@@ -261,127 +258,6 @@ function actualizarCantidad(index, cantidad) {
         mostrarTotalCompra();
     }
 }
-
-function agregarAlCarrito(producto, cantidad) {
-    let carritoData = localStorage.getItem("carrito");
-    carrito = carritoData ? JSON.parse(carritoData) : [];
-
-    let productoExistente = carrito.find((p) => p.nombre === producto.nombre);
-
-    if (productoExistente) {
-        let cantidadTotal = productoExistente.cantidad + cantidad;
-
-        if (cantidadTotal <= 100) {
-            productoExistente.cantidad = cantidadTotal;
-
-            if (cantidadTotal < 5) {
-                delete productoExistente.descuento;
-            } else if (!productoExistente.descuento) {
-                productoExistente.descuento = true;
-            }
-        } else {
-            alert("No se pueden agregar más de 100 unidades del producto: " + producto.nombre);
-            return;
-        }
-    } else {
-        producto.cantidad = cantidad;
-        carrito.push(producto);
-    }
-
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-
-    alert("Se ha agregado " + cantidad + " unidad(es) del producto '" + producto.nombre + "' al carrito.");
-
-    mostrarCarrito();
-    mostrarTotalCompra();
-}
-
-function mostrarCarrito() {
-    let carritoContainer = document.getElementById("carrito-container");
-    let carritoList = document.getElementById("carrito-list");
-    let totalCompra = document.getElementById("total-compra");
-    carritoList.innerHTML = "";
-    totalCompra.innerText = "";
-    let carritoData = localStorage.getItem("carrito");
-    let carrito = carritoData ? JSON.parse(carritoData) : [];
-
-    if (carrito.length === 0) {
-        carritoContainer.innerText = "El carrito está vacío.";
-    } else {
-        let subtotal = 0;
-        let descuentoTotal = 0;
-
-        for (let i = 0; i < carrito.length; i++) {
-            let producto = carrito[i];
-            let productoDiv = document.createElement("div");
-            productoDiv.classList.add("producto-carrito");
-
-            let productoImagen = document.createElement("img");
-            productoImagen.src = producto.imagen;
-            productoImagen.alt = producto.nombre;
-            productoImagen.classList.add("imagen-producto-carrito");
-
-            let productoNombre = document.createElement("h4");
-            productoNombre.innerText = producto.nombre;
-            productoNombre.classList.add("nombre-producto");
-
-            let cantidadLabel = document.createElement("label");
-            cantidadLabel.innerText = "Cantidad:";
-            cantidadLabel.classList.add("cantidad-label");
-
-            let cantidadSelect = document.createElement("input");
-            cantidadSelect.type = "number";
-            cantidadSelect.min = "1";
-            cantidadSelect.max = "100";
-            cantidadSelect.value = producto.cantidad;
-            cantidadSelect.classList.add("cantidad-select");
-            cantidadSelect.addEventListener("change", function () {
-                actualizarCantidad(i, parseInt(cantidadSelect.value));
-            });
-
-            let productoPrecio = document.createElement("p");
-            let descuento = 0;
-
-            if (producto.descuento) {
-                let descuentoPorUnidad = producto.precio * 0.2;
-                descuento = descuentoPorUnidad * producto.cantidad;
-                productoPrecio.innerText = "Precio unitario: $" + producto.precio.toFixed(2) + " (Descuento: $" + descuento.toFixed(2) + ")";
-                descuentoTotal += descuento;
-            } else {
-                productoPrecio.innerText = "Precio unitario: $" + producto.precio.toFixed(2);
-            }
-
-            let subtotalProducto = producto.precio * producto.cantidad - descuento;
-            subtotal += subtotalProducto;
-
-            let productoSubtotal = document.createElement("p");
-            productoSubtotal.innerText = "Subtotal: $" + subtotalProducto.toFixed(2);
-            productoSubtotal.classList.add("subtotal-producto");
-
-            let eliminarProductoButton = document.createElement("button");
-            eliminarProductoButton.innerText = "Eliminar";
-            eliminarProductoButton.classList.add("boton-eliminar");
-            eliminarProductoButton.addEventListener("click", function () {
-                eliminarProductoDelCarrito(i);
-            });
-
-            productoDiv.appendChild(productoImagen);
-            productoDiv.appendChild(productoNombre);
-            productoDiv.appendChild(cantidadLabel);
-            productoDiv.appendChild(cantidadSelect);
-            productoDiv.appendChild(productoPrecio);
-            productoDiv.appendChild(productoSubtotal);
-            productoDiv.appendChild(eliminarProductoButton);
-
-            carritoList.appendChild(productoDiv);
-        }
-
-        let total = subtotal - descuentoTotal;
-
-        totalCompra.innerText = "Total de la compra: $" + total.toFixed(2);
-    }
-}
-
 
 function mostrarTotalCompra() {
     let carrito = JSON.parse(localStorage.getItem("carrito"));
@@ -441,16 +317,6 @@ function mostrarSeleccionLicoreria() {
     licoreriaSeleccionada.appendChild(siguienteBtn);
 }
 
-// Mostrar el carrito al cargar la página
-window.addEventListener("load", function () {
-    mostrarCarrito();
-    document.getElementById("finalizar-compra-btn").addEventListener("click", function () {
-        mostrarTotalCompra();
-        document.getElementById("licoreria-seleccionada").style.display = "block";
-    });
-});
-
-
 function mostrarDatosLicoreria(licoreria) {
     let datosLicoreria = document.createElement("div");
     datosLicoreria.innerHTML = `
@@ -487,6 +353,7 @@ window.addEventListener("load", function () {
     mostrarCarrito();
     document.getElementById("finalizar-compra-btn").addEventListener("click", function () {
         mostrarTotalCompra();
+        document.getElementById("licoreria-seleccionada").style.display = "block";
     });
 });
 
